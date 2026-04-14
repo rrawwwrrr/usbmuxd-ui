980fd668a14a9638b0b6caf7e8d083ac06ff752c
Ruslan Ramazanov <ruslaramazanov@x5.ru>
2026-04-14 18:59:11 +0000

    fix: use HTTP MJPEG img instead of WebSocket for screen stream

--- src/app/service/hub.service.ts
+++ src/app/service/hub.service.ts
@@ -99,6 +99,7 @@ export class HubService {
         actions: [
           { type: 'pointerMove', duration: 0, x, y },
           { type: 'pointerDown', button: 0 },
+          { type: 'pause', duration: 100 },
           { type: 'pointerUp', button: 0 },
         ],
       }],
